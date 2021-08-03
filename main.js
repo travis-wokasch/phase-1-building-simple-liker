98b86d29af80ffd5cf239ab503b9dd3cc733f4df
Travis <tjwokasch@gmail.com>
2021-08-03 17:56:40 -0600

    commit tree fcebafc822abd884e779bb48df3c43c85f0101ee

--- main.js
+++ main.js
@@ -2,10 +2,45 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+
 // Your JavaScript code goes here!
 
+//add .hidden to error modal in HTML
+let errorModal = document.querySelector('#modal')
+errorModal.className = 'hidden'
 
+//click on empty heart button: span class ='like-glyph'
+const hearts = document.querySelectorAll('.like-glyph')
+//Invoke server call to similate request
 
+function liking(heart){
+  if(heart.innerText === EMPTY_HEART) {
+    heart.innerText = FULL_HEART
+    heart.className = 'activated-heart'
+  } else {
+    heart.innerText = EMPTY_HEART
+    heart.classList.remove('activated-heart')
+  }
+}
+
+hearts.forEach(heart => {
+    heart.addEventListener('click', () => {
+    mimicServerCall()
+      .then(res => {
+        liking(heart)
+      })
+      // .then(res => {
+      //   heart.innerText = EMPTY_HEART
+      //   heart.classList.remove('activated-heart')
+      // })
+      .catch(function(error) {
+        errorModal.hidden = false
+        alert('Random server error. Try again.')
+        document.getElementById('modal-message').innerText = error
+        setTimeout(() => errorModal.hidden = true, 3000)
+      })
+    })
+  })
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
